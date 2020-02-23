2165383d14e9e99a579941c316500edcef49a21f
Patrick Battisti <1729patrick@gmail.com>
2020-02-23 18:21:14 +0000

    feat: School is required to login

--- src/pages/public/Login/index.js
+++ src/pages/public/Login/index.js
@@ -15,7 +15,7 @@ const Login = ({ history }) => {
   const submitForm = async credentials => {
     try {
       const response = await api.post('/sessions', credentials);
-      const { user, token } = response.data;
+      const { user, token, school } = response.data;
 
       if (!user.active) {
         return history.push('/await_verification');
@@ -28,9 +28,7 @@ const Login = ({ history }) => {
 
       history.push('/dashboard');
     } catch ({ response }) {
-      toast.error(response?.data?.error || 'Invalid credentials', {
-        position: toast.POSITION.TOP_CENTER,
-      });
+      toast.error(response?.data?.error || 'Invalid credentials');
     }
   };
 
