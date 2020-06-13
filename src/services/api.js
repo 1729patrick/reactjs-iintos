dc2074def7fa2b1de56f6ab157367a53cca7753d
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-06-13 21:43:30 +0100

    api link

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
-      ? 'https://iintoska2.ips.pt',
+      ? 'https://iintoska2.ips.pt'
       : 'https://iintoska2.ips.pt',
 });
 
