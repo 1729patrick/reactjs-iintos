69c61f5d70e57ba03cac2b865b0c92b278490d3e
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-08-10 00:20:19 +0100

    events

--- src/pages/admin/Events/modals/Details/index.js
+++ src/pages/admin/Events/modals/Details/index.js
@@ -53,27 +53,72 @@ export default ({ open, setOpen, row }) => {
     setOpen(false);
   };
 
-  console.log(row);
+  const mountEvent11 = id => {
+    if (id !== 11) return;
 
-  //   createdAt: "2020-07-14T00:24:41.638Z"
-  // date: "2020-07-01T00:23:00.000Z"
-  // description: "The last transnational project meeting was held between 1 and 10 July 2020, via Zoom, with participants from all partner institutions. The main objective of this meeting was to carry out the joint evaluation of the project and the preparation of the final report.↵↵At the meeting, the partners were able to present / discuss the following topics over the course of 4 days, 1, 6, 7, 8 and 10 of July:↵- budget: expenses incurred / not yet incurred and respective proofs;↵- evaluation of intellectual products 1,2,3 and 4↵- evaluation of mobility and training events↵- evaluation of multiplier events↵- evaluation of dissemination activities,↵- assessment of the impact and sustainability of the project↵- discussion of the 1st draft of the final report"
-  // files: Array(1)
-  // 0:
-  // createdAt: "2020-07-14T00:24:32.755Z"
-  // id: 536
-  // link: null
-  // name: "IINTOS 3rd meeting agenda_July_2020.pdf"
-  // path: "0c32ecdb35babf032ef5a248a01e159b.pdf"
-  // updatedAt: "2020-07-14T00:24:32.755Z"
-  // url: "https://iintoska2.ips.pt/api/files/0c32ecdb35babf032ef5a248a01e159b.pdf"
-  // __proto__: Object
-  // length: 1
-  // __proto__: Array(0)
-  // id: 18
-  // sessions: Array(0)
+    return (
+      <>
+        <div
+          style={{
+            justifyContent: 'flex-start',
+            margin: '15px 0',
+            fontSize: 17,
+            textTransform: 'uppercase',
+          }}
+        >
+          News about this event
+        </div>
+        <div>
+          <a
+            href="https://www.si.ips.pt/ips_si/noticias_geral.ver_noticia?P_NR=7796"
+            target="_blank"
+          >
+            Conferência IINTOS | Projeto para agilizar mobilidade internacional
+            nas escolas
+          </a>
+          <br />
+          <br />
+          <a
+            href="https://issuu.com/ipsetubal/docs/jornalmovete_mai_jun2020"
+            target="_blank"
+          >
+            Jornal MOVEte | nº14 | maio/junho 2020
+          </a>
+        </div>
 
-  // Form controller
+        <iframe
+          key={1}
+          width="300"
+          height="150"
+          src="https://www.youtube.com/embed/DnOvxDtYpn0"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ marginTop: 25, borderRadius: 4, marginBottom: 10 }}
+        />
+      </>
+    );
+  };
+
+  const mountEvent10 = id => {
+    if (id !== 10) return;
+
+    return (
+      <>
+        <iframe
+          key={1}
+          width="300"
+          height="150"
+          src="https://www.youtube.com/embed/7P3_vypoEuQ"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ marginTop: 25, borderRadius: 4, marginBottom: 10 }}
+        />
+      </>
+    );
+  };
+
   return (
     <Modal
       aria-labelledby="simple-modal-title"
@@ -102,13 +147,16 @@ export default ({ open, setOpen, row }) => {
             />
           </p>
 
-          {!!row.files && (
+          {!!row.files?.length && (
             <FileList files={row.files} style={{ marginTop: 10 }} />
           )}
 
+          {mountEvent11(row.id)}
+          {mountEvent10(row.id)}
+
           {row.sessions.map((session, index) => (
             <>
-              <h3 style={{ marginTop: 10 }}>
+              <h3 style={{ marginTop: 10, marginBottom: 10 }}>
                 <b>Session {index + 1}</b>
               </h3>
               {session.description.trim().length > 0 && (
@@ -141,7 +189,7 @@ export default ({ open, setOpen, row }) => {
                   style={{ marginRight: 10, borderRadius: 4, marginBottom: 10 }}
                 />
               ))}
-              {session.files && (
+              {session.files?.length && (
                 <FileList files={session.files} style={{ marginTop: 10 }} />
               )}
             </>
--- src/pages/public/Project/Components/Products/index.js
+++ src/pages/public/Project/Components/Products/index.js
@@ -30,7 +30,7 @@ function Products() {
 
     const files = [
       {
-        name: 'Effective+strategy-1.pdf',
+        name: 'Effective strategy - 1.pdf',
         path: '0a866cab0d0bfe72c1c0e02ad7b3d6f9.pdf',
         updatedAt: '2020-08-06T00:18:05.701Z',
         url:
@@ -38,7 +38,7 @@ function Products() {
       },
 
       {
-        name: 'Effective+strategy-2.pdf',
+        name: 'Effective strategy - 2.pdf',
         path: 'a797d2f50dc027539904122147b7830a.pdf',
         updatedAt: '2020-08-06T00:17:36.548Z',
         url:
@@ -46,14 +46,14 @@ function Products() {
       },
 
       {
-        name: 'Effective strategy 3.pdf',
+        name: 'Effective strategy - 3.pdf',
         path: '7efa198a4b9bc52ff17a59b2c36df590.pdf',
         updatedAt: '2020-08-06T00:18:05.885Z',
         url:
           'https://iintoska2.ips.pt/api/files/7efa198a4b9bc52ff17a59b2c36df590.pdf',
       },
       {
-        name: 'Effective strategy 4.pdf',
+        name: 'Effective strategy - 4.pdf',
         path: 'f3ffc874bc6ee47f8a66fec8ce009ef0.pdf',
         updatedAt: '2020-08-06T00:18:04.684Z',
         url:
@@ -91,20 +91,43 @@ function Products() {
         url:
           'https://iintoska2.ips.pt/api/files/8d11f38253195379ed7ab467fb98e18a.pdf',
       },
+      {
+        name: 'Effective strategy - 9.pdf',
+        path: '478e253b40b502629416dd59205960ce.pdf',
+        updatedAt: '2020-08-09T16:46:13.833Z',
+        url:
+          'https://iintoska2.ips.pt/api/files/478e253b40b502629416dd59205960ce.pdf',
+      },
     ];
     return (
       <div>
+        <p>
+          This is a practical User guide focused on providing strategies and
+          pathways for schools to implement their own International Offices with
+          the help of the products and processes developed by the IINTOS
+          project.
+        </p>
         <p>
           <h3>This User guide has four main components:</h3>
         </p>
 
         <h4>1. Catalogue of Effective Internationalization Strategies;</h4>
-
+        <p>
+          This catalogue provides access to strategies that were successful,
+          including the ones from the IINTOS project pilot schools;
+        </p>
         <div style={{ marginLeft: 15 }}>
           <FileList files={files} />
         </div>
 
         <h4>2. Guide on Framework for Exchange;</h4>
+        <p>
+          On the Framework for Exchange document (on Curricula matrix tool),
+          there is a logical sequence of steps to take in order to start and
+          develop the process of internationalization. This guide provides help
+          on the use of the framework to find the main idea for an international
+          project, comparing selected relevant documents using content analyses;
+        </p>
 
         <div style={{ marginLeft: 15 }}>
           <a
@@ -116,10 +139,21 @@ function Products() {
           </a>
         </div>
         <h4>3. Platform tutorials;</h4>
+        <p>
+          A series of video tutorials on different aspect of the use of the
+          IINTOS project platform, with a strong focus on the development of
+          International Projects;
+        </p>
 
         <h4>
           4. Tutorials on implementation of international offices in schools.
         </h4>
+        <p>
+          Based on the experience and analysis on the entire process of
+          international office implementation by the IINTOS project, a series of
+          video were created focusing three key dimensions: strategic thinking,
+          operation and pedagogy.
+        </p>
 
         <div style={{ flexDirection: 'column', display: 'flex' }}>
           <iframe
