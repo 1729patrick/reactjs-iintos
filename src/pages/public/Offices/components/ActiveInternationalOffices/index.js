e5269c08946838af089b2c09ef07e1b0e89691ef
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-08-12 01:28:16 +0100

    active International Offices

--- src/pages/public/Offices/components/ActiveInternationalOffices/index.js
+++ src/pages/public/Offices/components/ActiveInternationalOffices/index.js
@@ -18,37 +18,39 @@ export default () => {
     <div>
       <h1>Active International Offices</h1>
       <div style={{ marginTop: 15, minWidth: '40%' }}>
-        {schools.map(school => (
-          <div
-            style={{
-              marginBottom: 20,
-              borderBottom: '1px solid #bbb',
-            }}
-          >
-            <p style={{ marginBottom: 5 }}>
-              <b>{school.name}</b>
-            </p>
-
-            {school.showContact && (
+        {schools.map(
+          school =>
+            school.showContact && (
               <div
                 style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  marginBottom: 10,
+                  marginBottom: 20,
+                  borderBottom: '1px solid #bbb',
                 }}
               >
-                <div>
-                  <a href={`mailto:${school.contactEmail}`}>
-                    {school.contactEmail}
-                  </a>
+                <p style={{ marginBottom: 5 }}>
+                  <b>{school.name}</b>
+                </p>
+
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    marginBottom: 10,
+                  }}
+                >
+                  <div>
+                    <a href={`mailto:${school.contactEmail}`}>
+                      {school.contactEmail}
+                    </a>
+                  </div>
                 </div>
+
+                <p>
+                  {school.city}, {school.postalCode},<br /> {school.country}
+                </p>
               </div>
-            )}
-            <p>
-              {school.city}, {school.postalCode},<br /> {school.country}
-            </p>
-          </div>
-        ))}
+            )
+        )}
       </div>
     </div>
   );
